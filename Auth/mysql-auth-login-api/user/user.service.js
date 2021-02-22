874c1beadd8c0ec3b476d73c03b6f50eafd1798f
Shangyi Dai <52154597+Sylvan1985@users.noreply.github.com>
2021-02-21 21:27:40 -0700

    Update user setting to match our database
    update default setting, can be used for testing now

---
+++ Auth/mysql-auth-login-api/user/user.service.js
@@ -0,0 +1,86 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function authenticate({ userEmail, userKey }) {
+    const user = await db.User.scope('withHash').findOne({ where: { userEmail } });
+
+    if (!user || !(await bcrypt.compare(userKey, user.userToken)))
+        throw 'userEmail or userKey is incorrect';
+
+    // authentication successful
+    const userToken = jwt.sign({ sub: user.userID }, config.secret, { expiresIn: '7d' });
+    return { ...omitHash(user.get()), userToken };
+}
+
+async function getAll() {
+    return await db.User.findAll();
+}
+
+async function getById(id) {
+    return await getUser(id);
+}
+
+async function create(params) {
+    // validate
+    if (await db.User.findOne({ where: { userEmail: params.userEmail } })) {
+        throw 'User"' + params.userEmail + '" is already taken';
+    }
+
+    // hash password
+    if (params.userKey) {
+        params.userToken = await bcrypt.hash(params.userKey, 10);
+    }
+
+    // save user
+    await db.User.create(params);
+}
+
+async function update(id, params) {
+    const user = await getUser(id);
+
+    // validate
+    const userEmailChanged = params.userEmail && user.userEmail !== params.userEmail;
+    if (userEmailChanged && await db.User.findOne({ where: { userEmail: params.userEmail } })) {
+        throw 'User"' + params.userEmail + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (params.userKey) {
+        params.userToken = await bcrypt.hash(params.userKey, 10);
+    }
+
+    // copy params to user and save
+    Object.assign(user, params);
+    await user.save();
+
+    return omitHash(user.get());
+}
+
+async function _delete(id) {
+    const user = await getUser(id);
+    await user.destroy();
+}
+
+// helper functions
+
+async function getUser(id) {
+    const user = await db.User.findByPk(id);
+    if (!user) throw 'User not found';
+    return user;
+}
+
+function omitHash(user) {
+    const { userToken, ...userWithoutHash } = user;
+    return userWithoutHash;
+}
